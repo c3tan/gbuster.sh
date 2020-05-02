ad4bc3c2f0461c447e4a32dbf89e2c8e9f4b0e30
Palash Verma <34809129+c3tan@users.noreply.github.com>
2020-05-02 15:30:32 +0530

    Update resolveIP.sh

--- resolveIP.sh
+++ resolveIP.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+# This script resolves ip for domains/subdomains from a file
+# Currently only for IPv4 but a simple modification and will work for IPv6
+# Subdomains should be given without protocols
 
 file=$1
 
 while IFS= read line
 do
         hostresolve=$(host $line |grep "has address" |cut -d' ' -f4)
-        echo "$(tput setaf 2)$line $(tput setaf 7) : $hostresolve" &
+        echo "$(tput setaf 2)$line $(tput setaf 7) : $hostresolve"
 done <"$file"
